cdf3738da095a26f19eed2e67bfeb918016f0c83
peterpickles <lipeter2006@gmail.com>
2017-12-13 09:58:10 -0800

    adding js to letter matching condition

---
+++ logic.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+// var letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
+
+//allow the divs to be injected with imgs
+
+//allow imgs to be dragged around
+
+//allow img to be dragged from one place to another
+
+//allow imgs to return to the origin point if not dragged to the desired place
+
+//Once the  
+
+$('.draggable-spans span').draggable();
+
+});
