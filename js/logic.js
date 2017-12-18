b6bae8d7944b7b041c677f15a95c9458a6cfb14b
peterpickles <lipeter2006@gmail.com>
2017-12-18 06:26:59 -0800

    changed js to jquery to work with draggable

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,7 @@
-var correctCards = 0;
 
+var correctCards = 0;
 $( init );
+
 function init() {
 
   // Hide the success message
@@ -31,125 +32,49 @@ function init() {
     } );
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var letterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var words = new Array;
-// words[0] = "CAT"
-// words[1] = "HAT"
-// words[2] = "BOY"
-// words[3] = "TOY"
-// var AnswerArray= [];
-
-// startGame();
-
-// //start game, grab a letter from the letterArr and put them inside each div
-// function startGame(){
-// 	var divAvailable = document.getElementById("letters");
-	
-// 	for(var i=0; i < letterArr.length; i++) {
-// 		var l = document.createElement('div');
-// 		l.innerHTML = letterArr[i];
-// 		l.className = "letterTiles";
-// 		divAvailable.appendChild(l);
-// 	}
-// }
-
-// //make elements draggable
-// $( function() {
-//     $(".letterTiles").draggable();
-// } )
-
-// // grab a word,, splice put it into an array
-// function stringToSplit(str) {
-
-// 	word = str.split ('');
-// 	AnswerArray.push(word);
-	
-// }
-// stringToSplit(words[0]);
-// console.log(AnswerArray);
-
-// //clear word to empty brackets
-// function clearWord () {
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //next word function
-// function nextWord {
-// 	console.log("Next word!")
-// } 
-//another function that has a for loop, run the stringSplit function in there
-//compare the content inside of the brackets
-
-
-
-// every time I drag letter, push() into an array
-// make a function to compare two arrays, if they match, then you win that instance
-// function >> letter I dragged, if that's not in the answer array, pop it out.
-// compare arrays --- might be using includes
-
-//allow img to be dragged from one place to another
-
-//allow imgs to return to the origin point if not dragged to the desired place
-
-
-// snap a piece of content to another piece of content 
-
-// assign something to each content when its clicked
-
-// when two content = same key/class/id are together
-
-// know when the content is not the correct content
-
-// check
-
-// if the letters are going to be a win case, when a letter is touching the correct word letter
-
-// make a button to go to the next word in the array
-
-// make a jquery logic for button showing after the word has been matched
-
+  // Create the card slots
+  var words = [ 'Apple', 'Boy', 'Cat', 'Dog', 'Elepant', 'Fort', 'Go', 'Hat', 'It', 'Jet', 'Key', 'Let', 'Monkey', 'Nancy', 'Ocean', 'Pet', 'Queen', 'Rat', 'Sam', 'Tea', 'Unicorn', 'Vee', 'Water', 'X-ray', 'Yosemite' , 'Zebra' ];
+  for ( var i=1; i<=26; i++ ) {
+    $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
+      accept: '#cardPile div',
+      hoverClass: 'hovered',
+      drop: cardDrop
+    } );
+  }
+
+}
+
+// If the card was dropped to the correct slot,
+  // change the card colour, position it directly
+  // on top of the slot, and prevent it being dragged
+  // again
+function cardDrop( event, ui ) {
+  var slotNumber = $(this).data( 'number' );
+  var cardNumber = ui.draggable.data( 'number' );
+
+  
+
+  if ( slotNumber == cardNumber ) {
+    ui.draggable.addClass( 'correct' );
+    ui.draggable.draggable( 'disable' );
+    $(this).droppable( 'disable' );
+    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    ui.draggable.draggable( 'option', 'revert', false );
+    correctCards++;
+  } 
+  
+  // If all the cards have been placed correctly then display a message
+  // and reset the cards for another go
+
+  if ( correctCards == 10 ) {
+    $('#successMessage').show();
+    $('#successMessage').animate( {
+      left: '380px',
+      top: '200px',
+      width: '400px',
+      height: '500px',
+      opacity: 1
+    } );
+  }
+
+}
