fde0890911e33a794fbe278e9ba7dbce87fb0767
peterpickles <lipeter2006@gmail.com>
2017-12-18 05:30:16 -0800

    changing some js into jquery

--- js/logic.js
+++ js/logic.js
@@ -1,44 +1,102 @@
-var letterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var words = new Array;
-words[0] = "CAT"
-words[1] = "HAT"
-words[2] = "BOY"
-words[3] = "TOY"
-var AnswerArray= [];
+var correctCards = 0;
 
-startGame();
+$( init );
+function init() {
 
-//start game, grab a letter from the letterArr and put them inside each div
-function startGame(){
-	var divAvailable = document.getElementById("letters");
+  // Hide the success message
+  $('#successMessage').hide();
+  $('#successMessage').css( {
+    left: '580px',
+    top: '250px',
+    width: 0,
+    height: 0
+  } );
+
+  // Reset the game
+  correctCards = 0;
+  $('#cardPile').html( '' );
+  $('#cardSlots').html( '' );
+
+  // Create the pile of shuffled cards
+  var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 ];
+  var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+  // <!--letters.sort( function() { return Math.random() - .5 } );-->
+
+  for ( var i=0; i<26; i++ ) {
+    $('<div>' + letters[i] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card'+numbers[i] ).appendTo( '#cardPile' ).draggable( {
+      
+      stack: '#cardPile div',
+      cursor: 'move',
+      revert: true
+    } );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var letterArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// var words = new Array;
+// words[0] = "CAT"
+// words[1] = "HAT"
+// words[2] = "BOY"
+// words[3] = "TOY"
+// var AnswerArray= [];
+
+// startGame();
+
+// //start game, grab a letter from the letterArr and put them inside each div
+// function startGame(){
+// 	var divAvailable = document.getElementById("letters");
 	
-	for(var i=0; i < letterArr.length; i++) {
-		var l = document.createElement('div');
-		l.innerHTML = letterArr[i];
-		l.className = "letterTiles";
-		divAvailable.appendChild(l);
-	}
-}
+// 	for(var i=0; i < letterArr.length; i++) {
+// 		var l = document.createElement('div');
+// 		l.innerHTML = letterArr[i];
+// 		l.className = "letterTiles";
+// 		divAvailable.appendChild(l);
+// 	}
+// }
 
-//make elements draggable
-$( function() {
-    $(".letterTiles").draggable();
-} )
+// //make elements draggable
+// $( function() {
+//     $(".letterTiles").draggable();
+// } )
 
-// grab a word,, splice put it into an array
-function stringToSplit(str) {
+// // grab a word,, splice put it into an array
+// function stringToSplit(str) {
 
-	word = str.split ('');
-	AnswerArray.push(word);
+// 	word = str.split ('');
+// 	AnswerArray.push(word);
 	
-}
-stringToSplit(words[0]);
-console.log(AnswerArray);
+// }
+// stringToSplit(words[0]);
+// console.log(AnswerArray);
 
-//clear word to empty brackets
-function clearWord () {
+// //clear word to empty brackets
+// function clearWord () {
 
-}
+// }
 
 
 
