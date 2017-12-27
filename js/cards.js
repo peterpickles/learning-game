2c5806b2a2ffb381d810e0c473829e404211a6a6
peterpickles <lipeter2006@gmail.com>
2017-12-27 11:42:21 -0800

    creating the cards object

--- js/cards.js
+++ js/cards.js
@@ -3,10 +3,34 @@ var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
   // <!--letters.sort( function() { return Math.random() - .5 } );--> didn't work :(
 var words = [ 'Apple', 'Boy', 'Cat', 'Dog', 'Elepant', 'Fort', 'Go', 'Hat', 'It', 'Jet', 'Key', 'Let', 'Monkey', 'Nancy', 'Ocean', 'Pet', 'Queen', 'Rat', 'Sam', 'Tea', 'Unicorn', 'Vee', 'Water', 'X-ray', 'Yosemite' , 'Zebra' ];
 
-var cards = [{ 
+var cards = [
+{ 
 	letters: "A"
 	words: "Apple"
-	
+}
 
+{ 
+	letters: "B"
+	words: "Boy"
+}
 
-}];
+{ 
+	letters: "C"
+	words: "Cat"
+}
+
+{ 
+	letters: "D"
+	words: "Dog"
+}
+
+{ 
+	letters: "E"
+	words: "Elepant"
+}
+{ 
+	letters: "F"
+	words: "Fort"
+}
+
+];
