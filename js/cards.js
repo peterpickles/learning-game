e3b2fc038633ab3a6783f7343e3bff04e89d12d5
peterpickles <lipeter2006@gmail.com>
2018-01-04 19:11:00 -0800

    omg, refactored my code to put all global variables and arrays in another file

--- js/cards.js
+++ js/cards.js
@@ -1,61 +1,65 @@
 var correctCards = 0;
-var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 ];
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
-  // <!--letters.sort( function() { return Math.random() - .5 } );--> didn't work :(
 var words = [ 'Apple', 'Boy', 'Cat', 'Dog', 'Elepant', 'Fort', 'Go', 'Hat', 'It', 'Jet', 'Key', 'Let', 'Monkey', 'Nancy', 'Ocean', 'Pet', 'Queen', 'Rat', 'Sam', 'Tea', 'Unicorn', 'Vee', 'Water', 'X-ray', 'Yosemite' , 'Zebra' ];
+var num = Math.floor(Math.random()*25)
+var randomLetter= letters[num];
+var correctWord = words[num];
+var randomWord = words[Math.floor(Math.random()*25)];
+// var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 ];
 
-var cards = [
-{ 
-	letters: "A",
-	words: "Apple"
-},	
-{ 
-	letters: "B",
-	words: "Boy"
-},
-{ 
-	letters: "C",
-	words: "Apple"
-},
-{ 
-	letters: "D",
-	words: "word"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-},
-{ 
-	letters: "",
-	words: "Apple"
-}
 
-];
+// var cards = [
+// { 
+// 	letters: "A",
+// 	words: "Apple"
+// },	
+// { 
+// 	letters: "B",
+// 	words: "Boy"
+// },
+// { 
+// 	letters: "C",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "D",
+// 	words: "word"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// },
+// { 
+// 	letters: "",
+// 	words: "Apple"
+// }
+
+// ];
--- js/logic.js
+++ js/logic.js
@@ -1,60 +1,73 @@
-var correctCards = 0;
 $( init );
+$( showLetters);
+$( showWords);
 
 function init() {
-
-  // Hide the success message
-  $('#successMessage').hide();
-  $('#successMessage').css( {
+//Hide the 
+ $('#successMessage').hide();
+ $('#successMessage').css( {
     left: '580px',
     top: '250px',
     width: 0,
     height: 0
   } );
+// Reset the game, sets the cards back to its position
+correctCards = 0;
+  // $('#cardPile').html( '' );
+  // $('#cardSlots').html( '' );
+}
 
-  // Reset the game, sets the cards back to its position
-  correctCards = 0;
-  $('#cardPile').html( '' );
-  $('#cardSlots').html( '' );
 
-  //Made a for loop to go through the letters array
-  //made a div with the letter based on it's index in the array
-  for ( var i=0; i<26; i++ ) {
-    $('<div>' + letters[i] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card'+numbers[i] ).appendTo( '#cardPile' ).draggable( {
+//Made a for loop to go through the letters array
+//made a div with the letter based on it's index in the array
+function showLetters() {
+for ( var i=0; i<1; i++ ) {
+    $('<div>' + randomLetter + '</div>').appendTo( '#cardPile' ).draggable( {
       
       stack: '#cardPile div',
       cursor: 'move',
       revert: true
     } );
   }
-
-  // creating the word cards
-  
-  for ( var i=1; i<=26; i++ ) {
-    $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
-      accept: '#cardPile div',
-      hoverClass: 'hovered',
-      drop: cardDrop
-    } );
-  }
+}
+// creating the word cards
+function showWords() {
+  // for ( var i=1; i<=2; i++ ) {
+  //   $('<div>' +  + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable({
+  //     accept: '#cardPile div',
+  //     hoverClass: 'hovered',
+  //     drop: cardDrop
+  //   });
+  // }
+      $('<div>' + randomWord + '</div>').appendTo("#cardSlots").droppable({
+          accept: '#cardSlots div',
+          hoverClass: 'hovered',
+          drop: cardDrop
+      });
+      $('<div>' + words[num] + '</div>').appendTo("#cardSlots").droppable({
+          accept: '#cardSlots div',
+          hoverClass: 'hovered',
+          drop: cardDrop
+      });
 
 }
 
-// If the card was dropped to the correct slot,
- 
-function cardDrop( event, ui ) {
-  var slotNumber = $(this).data( 'number' );
-  var cardNumber = ui.draggable.data( 'number' );
+function cardDrop() {
 
-  if ( slotNumber == cardNumber ) {
-    ui.draggable.addClass( 'correct' );
-    ui.draggable.draggable( 'disable' );
-//once the card is confirmed position it directly on top of the slot, and prevent it being dragged
-    $(this).droppable( 'disable' );
-    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
-    ui.draggable.draggable( 'option', 'revert', false );
-    correctCards++;
-  } 
+}
+// function cardDrop( event, ui ) {
+//   var slotNumber = $(this).data( 'number' );
+//   var cardNumber = ui.draggable.data( 'number' );
+
+//   if ( slotNumber == cardNumber ) {
+//     ui.draggable.addClass( 'correct' );
+//     ui.draggable.draggable( 'disable' );
+// //once the card is confirmed position it directly on top of the slot, and prevent it being dragged
+//     $(this).droppable( 'disable' );
+//     ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+//     ui.draggable.draggable( 'option', 'revert', false );
+//     correctCards++;
+//   } 
   
 // if 10 cards are placed correctly and accepted, populate successmessage
 
